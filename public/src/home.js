24abb806097b284e0c39fb60b70c3af8560b4213
Mike <scnd30@gmail.com>
2022-02-18 00:12:29 -0500

    Updated qualified assignment to include .reduce method

--- public/src/home.js
+++ public/src/home.js
@@ -7,8 +7,7 @@ function getTotalAccountsCount(accounts) {
 }
 
 function getBooksBorrowedCount(books) {
-  let booksCheckedOut = books.filter(
-    (book) =>
+  let booksCheckedOut = books.filter((book) =>
       book.borrows.filter((record) => record.returned === false).length > 0
   );
   return booksCheckedOut.length;
@@ -43,22 +42,20 @@ function getMostPopularBooks(books) {
    .slice(0, 5);
  }
 
- function getMostPopularAuthors(books, authors) {
-  let result = [];
-  authors.forEach((author) => {
-   let theAuthor = {
-    name: `${author.name.first} ${author.name.last}`,
-    count: 0
-   };
-   books.forEach((book) => {
-    if (book.authorId === author.id) {
-     theAuthor.count += book.borrows.length;
-    }
-   });
-   result.push(theAuthor);
-  });
-  return result.sort((a, b) => b.count - a.count).slice(0, 5);
- }
+
+function getMostPopularAuthors(books,authors) {
+  const result = authors.map(author => ({
+  ...author,
+  bookCount: books.filter(book => book.authorId === author.id).length,
+  borrowCount: books.filter(book => book.authorId === author.id).reduce((acc, cur) => acc + cur.borrows.length, 0)})).sort((b, a) => a.borrowCount - b.borrowCount);
+  result.length = 5;
+  return result.map(result => {
+  return {count: result.borrowCount, name: result.name.first + " " + result.name.last};
+  })
+}
+
+
+
 
 module.exports = {
   getTotalBooksCount,
